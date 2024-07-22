9543a3ae13d90ecf9c5a3f4faf26a5db2885ee49
Madi Bromfield <madibromfield215@gmail.com>
2024-07-22 12:59:45 -0500

    add new files

---
+++ script.js
@@ -0,0 +1,18 @@
+//  message popup
+//  combination problems *see if you can use multiple variables*
+//  dialog box/alert
+
+// message
+const que =
+  "You have recieved this message because you have been chosen to open and important vault. Here is the secret combination:";
+console.log(que);
+// combination
+const combo1 = 22 - (27 - 3) / 2;
+console.log(combo1);
+const combo2 = 2 * 15 + 10;
+console.log(combo2);
+const combo3 = (15 + 4) * 3 - 18;
+console.log(combo3);
+// popup dialog
+alert(que);
+alert(combo1 + " " + combo2 + " " + combo3);
