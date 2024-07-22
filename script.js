1c16ae397370f03906af75d49c335d9cc0b83e4a
Madi Bromfield <madibromfield215@gmail.com>
2024-07-22 17:18:52 -0500

    add new files

--- script.js
+++ script.js
@@ -3,16 +3,16 @@
 //  dialog box/alert
 
 // MESSAGE
-// const que =
-//   "You have recieved this message because you have been chosen to open and important vault. Here is the secret combination:";
-// console.log(que);
-// // COMBINATION
-// const combo1 = 22 - (27 - 3) / 2;
-// console.log(combo1);
-// const combo2 = 2 * 15 + 10;
-// console.log(combo2);
-// const combo3 = (15 + 4) * 3 - 18;
-// console.log(combo3);
-// // POPUP DIALOG
-// alert(que);
-// alert(combo1 + " " + combo2 + " " + combo3);
+const que =
+  "You have recieved this message because you have been chosen to open and important vault. Here is the secret combination:";
+console.log(que);
+// COMBINATION
+const combo1 = 22 - (27 - 3) / 2;
+console.log(combo1);
+const combo2 = 2 * 15 + 10;
+console.log(combo2);
+const combo3 = (15 + 4) * 3 - 18;
+console.log(combo3);
+// POPUP DIALOG
+alert(que);
+alert(combo1 + " " + combo2 + " " + combo3);
